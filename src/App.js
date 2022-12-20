24683ac33f73c80d6638a432a6443ab1101c4ab9
KentaroKoizumi <kentaroskoizumi@gmail.com>
2022-12-20 10:50:56 +0900

    google translate が新しいタブで開くようにした

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { createWorker } from 'tesseract.js';
 import './App.css';
 import { useState, useEffect } from 'react';
+import { Box } from '@mui/system';
+import { CircularProgress, Typography } from '@mui/material';
 
 function App() {
   const [src, setSrc] = useState();
-  const [hungul, setHungul] = useState();
+  const [hangul, sethangul] = useState();
+  const [ocrProgress, setOcrProgress] = useState({status: 'recognizing text', progress: 1})
   const handleOnPaste = (event) => {
     console.log({ event });
     const items = (event.clipboardData || event.originalEvent.clipboardData)
@@ -32,45 +35,69 @@ function App() {
       console.log({ reader });
     }
   };
-  const worker = createWorker({
-    logger: (m) => console.log(m),
-  });
-  // const recognize = async () => {
-  //   console.log(worker)
-  //   await worker.load();
-  //   await worker.loadLanguage('eng');
-  //   await worker.initialize('eng');
-  //   const { data: { text } } = await worker.recognize(src);
-  //   setHungul(text);
-  //   console.log("a")
-  //   console.log(text);
-  //   await worker.terminate();
-  // };
+  
   useEffect( () =>{
+    const worker = createWorker({
+      logger: (m) => setOcrProgress({status: m.status, progress: m.progress}),
+    });
     console.log(src)
     async function recognize(){
       await worker.load();
-      await worker.loadLanguage('eng+kor');
-      await worker.initialize('eng+kor');
+      await worker.loadLanguage('kor');
+      await worker.initialize('kor');
       const { data: { text } } = await worker.recognize(src);
-      setHungul(text);
+      sethangul(text.replaceAll(/\s+/g, ''));
       console.log(text);
       await worker.terminate();
     };
     recognize();
   }, [src]);
   return (
-    <>   
-      <h1>ハングルチェッカー</h1>
-      <span>ハングルの画像を貼り付けろ！</span>
-      <br />
-      <br />
-      <textarea onPaste={handleOnPaste} />
-      <br />
-      <p>{hungul}</p>
-      <br />
-      <img src={src} alt="pic" />
-      {/* <button onClick={recognize}>認識</button> */}
+    <>
+    <Box sx={{height:"10em"}}/>
+      <Box
+        sx={{
+          justifyContent: "center", 
+          textAlign: "center",
+        }}
+      >
+        <Typography
+          variant="h3"
+          >
+          ハングルチェック
+        </Typography>
+        <span>ハングルの画像を貼り付けろ！</span>
+        <br />
+        <br />
+        <textarea onPaste={handleOnPaste} />
+        <br />
+        { !(ocrProgress.progress === 1 && (ocrProgress.status === "recognizing text" || ocrProgress.status === "initialized api")) &&
+          <Box
+            sx={{
+              width: "20em",
+              margin: "auto",
+            }}
+          >
+            <CircularProgress variant="determinate" value={ocrProgress.progress * 100} />
+          </Box>
+        }
+        {
+          hangul &&
+          <>
+            <p>{hangul}</p>
+            <a
+            href={`https://translate.google.com/?sl=ko&tl=ja&text=${hangul}&op=translate`}
+            target="_blank" rel="noopener noreferrer"
+            >
+              Google 翻訳へ
+            </a>
+          </>
+        }
+        <br />
+        {
+          src && <img src={src} alt="a"/>
+        }
+      </Box>
     </>
   );
 }
